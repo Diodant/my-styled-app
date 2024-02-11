dbf586e67030eeab9d63bd92480e0ae9bab0bfd5
Nick Tolok <97984634+Diodant@users.noreply.github.com>
2024-02-11 21:10:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { StyledContainer, StyledButton, StyledPage, StyledButtonContainer, StyledTitle, StyledText, StyledImg, StyledImgContainer } from './components/StyledComponent';
 import Image from './img/images.png';
 import YesPage from './components/YES';
